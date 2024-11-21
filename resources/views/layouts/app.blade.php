97d68b89cf1c72759a4d7eb24683accc44800dfc
franciscor2 <1190520@usap.edu>
2024-11-20 23:54:32 -0600

    rediseño del login y registro y se modifico el welcome – jose francisco R.
    Se rediseño el login y registro y se modifico el welcome para creear la pagina de la empresa ficticia.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ asset('images/logo.png') }}" alt="Logo" style="max-height: 40px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -78,3 +79,4 @@
     </div>
 </body>
 </html>
+
